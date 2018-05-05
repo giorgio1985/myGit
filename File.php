a01891e47c67b2827228b38f2a770f9771479f65
giorgio1985 <g.adonoo@yahoo.it>
2018-05-05 23:31:49 +0000

    da rivedere

--- File.php
+++ File.php
@@ -5,16 +5,24 @@ include 'dbConnector.php';
 if(isset($_POST['username'])&&($_POST['email'])){
 $username = $_POST['username'];
 $email = $_POST['email'];
-$sql="SELECT username, email FROM firstLogin WHERE username=$username AND $email=$email";
-mysqli_query($conn,$sql);
-if($sql){
+$sql="SELECT username, email FROM firstLogin; # WHERE username=$username AND email=$email";
+$result=mysqli_query($conn,$sql);
+if($result){
 echo 'Benvenuto  ' .$_POST['username'];
-}else
-echo 'Are you just logged?';
-
 }
+else {    echo 'Are you just logged?';   }
 
+//if (mysqli_num_rows($out)>0) {
+
+if (mysqli_num_rows($result) > 0) {
+while ($row=mysqli_fetch_assoc($result)) {
+	echo $row['username']; 
+	echo '<br>';
+	echo $row['email'];
+	echo '<br>';
+   }
+}
+}
+//mysqli_free_result($out);
 mysqli_close($conn);
-
-
 ?>
