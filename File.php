57a0b621a072d341001b6bff1eedc2d5db87368f
giorgio1985 <g.adonoo@yahoo.it>
2018-05-05 23:56:47 +0000

    revisioned

--- File.php
+++ File.php
@@ -6,6 +6,10 @@ if(isset($_POST['username'])&&($_POST['email'])){
 $username = $_POST['username'];
 $email = $_POST['email'];
 $sql="SELECT nome, cognome, username, email FROM firstLogin WHERE username='$username' AND email='$email'";
+if (!$sql) {
+	header('location: File.html');
+}
+
 $result=mysqli_query($conn,$sql);
 if($result){
 echo 'Benvenuto  ' .$_POST['username'];
@@ -16,6 +20,7 @@ else {    echo 'Are you just logged?';   }
 
 if (mysqli_num_rows($result) > 0) {
 while ($row=mysqli_fetch_assoc($result)) {
+	echo '<br>';
 	echo $row['username']; 
 	echo '<br>';
 	echo $row['email'];
@@ -23,7 +28,9 @@ while ($row=mysqli_fetch_assoc($result)) {
 	echo $row['nome'];
 	echo '<br>';
 	echo $row['cognome'];
+	echo '<br>';
 	echo '-------------';
+	echo '<br>';
    }
 }
 }
